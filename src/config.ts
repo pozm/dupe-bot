7cf0d58d225d789beeec2f38b2fddcbc87f3f8e6
pozm <44528100+pozm@users.noreply.github.com>
2021-04-21 22:17:07 +0100

    fix broken workers2

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@
 
 
 export const config = {
-	// host: 'slupe.minehut.gg',
-	host: '5.68.139.87',
+	host: 'slupe.minehut.gg',
+	// host: '5.68.139.87',
 	// host: 'localhost',
-	interval: 1e3
+	interval: 3e3
 }
